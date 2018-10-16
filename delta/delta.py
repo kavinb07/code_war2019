50b53f669270e5abd2b3dec00eb67c11613ff8f3
Kavin Balakrishnan <kavinb07@gmail.com>
2018-10-16 07:03:55 -0700

    This is a delta message

--- delta/delta.py
+++ delta/delta.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
-##Sample Test upload
-##changes
+
 def delta(line_str):
-    print line_str
+    list_deltas = []
+    num = line_str.split()
+    for iter in range(1, len(num) - 1):
+        diff = int(num[iter + 1]) - int(num[iter])
+        list_deltas.append(-diff)
+
+    first_val = int(num[1])
+    string_line = str(first_val)
+    for delta_value in list_deltas:
+        first_val = first_val + delta_value
+        string_line = string_line + " " + str(first_val)
+    print string_line
+
+
 
 with open("delta.txt", "r") as f:
     data_l = f.readlines()
