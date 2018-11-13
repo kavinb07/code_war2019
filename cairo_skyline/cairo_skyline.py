9edad6454c63ab7b4239a3b3d787957bd685890e
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2018-11-12 18:50:14 -0800

    add comments for better readability

--- cairo_skyline/cairo_skyline.py
+++ cairo_skyline/cairo_skyline.py
@@ -19,6 +19,7 @@ def print_matrix(full_matrix):
     for col in full_matrix:
         row_string = ''
         for cell in col:
+            # the matrix does not display 0, as it is the filler of the pyramids
             if cell != '0':
                 row_string += cell
             else:
@@ -27,8 +28,17 @@ def print_matrix(full_matrix):
 
 # function gets all points for a specific value pair
 def get_points(center, height):
-    m = 1
     x_int = []
+    # each point is a dictionary with three attributes: x, y, and display
+    #
+    # x and y are simply coordinates sent to matrix. They should have values
+    # that are positive integers.
+    #
+    # display is the character that shows up on the matrix. Forward slashes
+    # and backslashes are used for the edges of the pyramid, while an underscore
+    # is used for the top of a pyramid and a zero is used for the inside of a
+    # pyramid. Zeros are not printed. Points with a display of 0 are called
+    # filler points
     points = [
         {
             'x': center,
@@ -36,33 +46,48 @@ def get_points(center, height):
             'display': '_'
         }
     ]
+    # this loop puts filler points in the middle column of the pyramid
     for y in range(height):
         points.append({
             'x': center,
             'y': y,
             'display': '0'
         })
+    # the next two lines put the two x-intercepts of a pyramid into a list
     x_int.append(center - height)
     x_int.append(center + height)
-
+    # this loop runs one time for each x-intercept.
     for i in range(2):
         x = x_int[i]
         if i == 0:
+            # if i == 0, the first x-intercept is being used, which means
+            # the value of b in y=mx+b is negative and that the slope of the
+            # line will be positive
             b = -1 * x
         else:
+            # i == 1, so the second x-intercept is being used, which means that
+            # the value of b in y=mx+b is positive and that the slope of the
+            # line is negative
             b = 1 * x
+        # this loop adds the edge points to the array of points
         for y in range(height):
+            # this system solves for x in terms of y, so the new_x variable is
+            # from the equation x=(y-b)/m. y is the iteration in the loop, m is
+            # +/-1, and b is -1(x-int)
             if i == 1:
                 new_x = -1 * (y - b)
+                # because the slope is -1 and the pyramid is going down, use \
                 display_for_use = '\\'
             else:
                 new_x = y - b
+                # use / because the slope is +1 and the pyramid is going up
                 display_for_use = '/'
             points.append({
                 'x': new_x,
                 'y': y,
                 'display': display_for_use
             })
+            # add filler characters
             for alt_y in range(y):
                 points.append({
                     'x': new_x,
