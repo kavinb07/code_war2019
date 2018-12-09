7b685a7e2fb73372dc60676b251a809949df1c01
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-08 17:45:46 -0800

    disassemble initial solution

---
+++ disassemble/disassemble.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+with open('disassemble.txt') as f:
+    data = f.readlines()
+
+
+special_strings = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+    'eight', 'nine', 'ten'
+]
+
+print('Number ' + str(special_strings[int(data[0])]) + ' is alive!')
