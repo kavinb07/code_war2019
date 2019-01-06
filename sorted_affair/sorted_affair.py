4724f54294c762bac92f11c4951ff73ccbd3b56f
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2019-01-05 16:05:16 -0800

    :joy:

--- sorted_affair/sorted_affair.py
+++ sorted_affair/sorted_affair.py
@@ -1,24 +1,42 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
+import operator
 
 with open('sorted_affair.txt') as f:
     data = f.readlines()
 
 
-ranks = {}
-
+teams = {}
+# populate teams
 for line in data:
     if line.rstrip() == '0 0':
         break
-    ranks['a' + line.split()[0]] = 0
+    teams['a' + line.split()[0]] = 0
 
-
+# add up points
 for line in data:
     if line.rstrip() == '0 0':
         break
     values = line.split()
     team = values[0]
     points = int(values[1])
-    ranks['a' + team] += points
+    teams['a' + team] += points
 
 
-print(ranks)
+ranks = [{'name': 'q', 'score': 0}, {'name': 'q', 'score': 0},
+         {'name': 'q', 'score': 0}, {'name': 'q', 'score': 0},
+         {'name': 'q', 'score': 0}]
+for team, points in reversed(sorted(teams.items(), key=operator.itemgetter(1))):
+    for i in range(len(ranks)):
+        if points > ranks[i]['score']:
+            ranks[i]['name'] = team
+            ranks[i]['score'] = points
+            break
+
+
+for i in range(len(ranks)):
+    string = ''
+    string += str(i + 1) + ' '
+    string += ranks[i]['name'][1:5] + ' '
+    string += str(ranks[i]['score'])
+    print(string)
