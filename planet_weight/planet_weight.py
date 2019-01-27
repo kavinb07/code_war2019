8c1eb273dcdd431272bbd526fb916b2bb057d58b
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2019-01-26 16:57:25 -0800

    initial solution

---
+++ planet_weight/planet_weight.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+with open('planet_weight.txt') as f:
+    data = f.readlines()
+
+
+for line in data:
+    values = line.split()
+    name = values[0]
+    if name == 'END':
+        break
+    weight = float(values[1])
+    planet = values[2]
+    conversion = float(values[3])
+    print('On ' + planet + ', ' + name + ' would weigh ' + str(weight * conversion)) + ' pounds.'
